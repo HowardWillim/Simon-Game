d99ea20fc20d926c2f69e6c86d4a3e4b0ed69753
HowardWillim <kurolimzz@gmail.com>
2023-02-04 20:54:41 +0700

    working on the game logic

--- game.js
+++ game.js
@@ -1,47 +1,72 @@
 var gamePattern=[];
+var userClickedPattern=[];
+var level =0;
+var started=false;
+
 
 
 var buttonColours = ["red", "blue", "green", "yellow"];
-var randomChosenColour =buttonColours[nextSequence()];
-
-
-
-
-
-
-
 
 function nextSequence(){
-    var randomNumber = Math.floor(Math.random()*3) + 1;
-    return randomNumber
+    var randomNumber = Math.floor(Math.random()*4) ;
+    var randomChosenColour =buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColour);
+    level+=1;
+    $("#level-title").text("Level " + level);
+    
+    
+}
+
+$(".btn").click(function(){
+    var userChosenColour = this.id;
+    userClickedPattern.push(userChosenColour);
+    
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+    checkAnswer(userClickedPattern.length-1);
+})
+
+
+
+
+function checkAnswer(currentLevel){
+    if(userClickedPattern[currentLevel] === gamePattern[currentLevel]){
+        if(userClickedPattern.length === gamePattern.length ){
+            setTimeout(function(){
+                nextSequence();
+                userClickedPattern=[];
+            },1000);
+        }
+    }
+
 }
 
 
-
-gamePattern.push(randomChosenColour);
-
-$("#"+randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100)
-
-function playSound(colour){
-    switch (colour) {
-        case "red":
-            var red=new Audio("sounds/red.mp3")
-            red.play();
-            break;
-        case "blue":
-            var blue=new Audio("sounds/blue.mp3")
-            blue.play();
-            break;
-        case "green":
-            var green=new Audio("sounds/green.mp3")
-            green.play();
-            break;
-        case "yellow":
-            var yellow=new Audio("sounds/yellow.mp3")
-            yellow.play();
-            break;
     
-        default:
-            break;
+$(document).keypress(function() {
+    if (!started) {
+  
+      //3. The h1 title starts out saying "Press A Key to Start", when the game has started, change this to say "Level 0".
+      $("#level-title").text("Level " + level);
+      nextSequence();
+      started = true;
     }
+  });
+
+
+
+
+
+function playSound(name) {
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+  }
+
+function animatePress(currentColour){
+    $("#"+currentColour).addClass("pressed")
+    setTimeout(function(){
+         $("#"+currentColour).removeClass("pressed")},100)
+    
 }
